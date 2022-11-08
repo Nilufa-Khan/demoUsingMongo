2f0a1d9628ce20f68b497debd75d2dddf6310d11
Nilufa Khan <khannilufa5@gmail.com>
2022-11-08 12:46:11 +0530

    Initial commit

--- src/test/java/com/example/demoUsingMongo/repository/CustomerRepositoryTest.java
+++ src/test/java/com/example/demoUsingMongo/repository/CustomerRepositoryTest.java
@@ -4,13 +4,20 @@ import com.example.demoUsingMongo.domain.Customer;
 import com.sun.jdi.PrimitiveValue;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 @ExtendWith(SpringExtension.class)
 @DataMongoTest
 public class CustomerRepositoryTest {
+    @Autowired
 private CustomerRepository customerRepository;
 private Address address;
 private Customer customer;
@@ -26,5 +33,13 @@ public void tearDown(){
     address = null;
     customer = null;
     customerRepository.deleteAll();
+}
+@Test
+@DisplayName("Test case for saving customer object")
+void givenCustomerToSaveShouldReturnSavedCustomer(){
+    customerRepository.save(customer);
+    Customer customer1 = customerRepository.findById(customer.getCustomerId()).get();
+    assertNotNull(customer1);
+    assertEquals(customer.getCustomerId(),customer1.getCustomerId());
 }
 }
