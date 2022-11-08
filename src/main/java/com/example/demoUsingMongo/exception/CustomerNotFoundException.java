8fe92a1ecaabddbb39eb200ebb8a87c829f4d579
Nilufa Khan <khannilufa5@gmail.com>
2022-11-08 09:45:20 +0530

    Initial commit

--- src/main/java/com/example/demoUsingMongo/exception/CustomerNotFoundException.java
+++ src/main/java/com/example/demoUsingMongo/exception/CustomerNotFoundException.java
@@ -5,5 +5,4 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(code = HttpStatus.NOT_FOUND,reason = "customer id is not found which is given by you")
 public class CustomerNotFoundException extends Exception{
-
 }
