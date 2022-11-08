0ebee153ecffb9fe7ec0ffe7a484422538ec12b8
Nilufa Khan <khannilufa5@gmail.com>
2022-11-08 09:46:20 +0530

    Initial commit

--- src/main/java/com/example/demoUsingMongo/service/CustomerService.java
+++ src/main/java/com/example/demoUsingMongo/service/CustomerService.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public interface CustomerService {
     Customer saveCustomer(Customer customer);
+
     List<Customer> getAllCustomerData() throws CustomerNotFoundException;
     public boolean deleteCustomer(int cusId) throws CustomerNotFoundException;
     List<Customer> getAllCustomerByCity(String city) throws CustomerNotFoundException;
